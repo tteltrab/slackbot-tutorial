ddeb236aef00423b95b07d0e380d8abc2397312f
Nick Bartlett <nbartlett7@gmail.com>
2019-05-02 15:10:17 -0400

    fix server.py

--- server.py
+++ server.py
@@ -1,27 +1,27 @@
 from flask import Flask, request, make_response, Response
 import json
-from tokens import SLACK_BOT_TOKEN, SLACK_VERIFICATION_TOKEN
+import os
 from slackclient import SlackClient
 from slashCommand import *
 
-
-
-slack_client = SlackClient(SLACK_BOT_TOKEN)
+slack_client = SlackClient(os.environ['SLACK_BOT_TOKEN'])
 app = Flask(__name__)
 
 commander = Slash("Hey there! It works.")
 
 #TODO: Add checks for all responses from slack api calls
 
-def verify_slack_token(request_token):
-    if SLACK_VERIFICATION_TOKEN != request_token:
-        print("Error: invalid verification token!")
-        print("Received {} but was expecting {}".format(request_token, SLACK_VERIFICATION_TOKEN))
-        return make_response("Request contains invalid Slack verification token", 403)
+# def verify_slack_token(request_token):
+#     if SLACK_VERIFICATION_TOKEN != request_token:
+#         print("Error: invalid verification token!")
+#         print("Received {} but was expecting {}".format(request_token, SLACK_VERIFICATION_TOKEN))
+#         return make_response("Request contains invalid Slack verification token", 403)
 
-@app.route("/slack/test", methods=["POST"])
+@app.route("/slack/ask", methods=["POST"])
 def command():
   info = request.form
+
+  print(info)
 
   # # get uid of the user
   # im_id = slack_client.api_call(
@@ -40,6 +40,7 @@ def command():
     "chat.postMessage",
     channel="#" + info["channel_name"],
     text=commander.getMessage()
+  )
 
   return make_response("", 200)
 
